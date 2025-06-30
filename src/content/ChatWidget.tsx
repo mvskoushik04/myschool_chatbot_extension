6b768bb1f8a5e7a39b163e9af2ba3c2f5551a28c
MVS Koushik <medurivskoushik@gmail.com>
2025-06-30 10:49:28 +0530

    sixth commit

--- src/content/ChatWidget.tsx
+++ src/content/ChatWidget.tsx
@@ -419,9 +419,7 @@ const ChatWidget: React.FC = () => {
                 justifyContent: 'center'
               }}
             >
-              <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M2.01 21L23 12L2.01 3V10L17 12L2.01 14V21Z" fill="white" stroke="#409eff" strokeWidth="2" strokeLinejoin="round"/>
-              </svg>
+            
             </button>
           </div>
         </>
